271510c39ba03ec934be20dcd85189521ea73c3e
MHLiuLGY <10222150413@stu.ecnu.edu.cn>
2023-12-28 10:50:36 +0800

    report3

--- Hardware/IR.c
+++ Hardware/IR.c
@@ -77,54 +77,54 @@ uint8_t IR_GetCommand(){
 }
 
 void EXTI2_IRQHandler(){
-	if(EXTI_GetITStatus(EXTI_Line2)==SET){
-		EXTI_ClearITPendingBit(EXTI_Line2);
-		switch(IR_State){
-			case 0:{//第1个下降沿，新的一帧，进入状态1
-				TIM_SetCounter(TIM2,0);
-				TIM_Cmd(TIM2,ENABLE);
-				IR_State=1;
-				break;
-			}
-			case 1:{//第2个下降沿，判断开始或重复。若判断为开始，进入状态2；若判断为重复，该帧结束，回到状态1
-				IR_Time=IRTimer_GetCounter();
-				TIM_SetCounter(TIM2,0);//不关闭计时器，且开始新的计时
-				if(IR_Time>13500-500 && IR_Time<13500+500){
-					IR_State=2;
-				}else if(IR_Time>11250-500 && IR_Time<11250+500){
-					IR_RepeatFlag=1;
-					TIM_Cmd(TIM2,DISABLE);//也可以不停计时器，在状态0会清零重启
-					IR_State=0;
-				}else{
-					IR_State=1;
-				}
-				break;
-			}
-			case 2:{//第3个至n个下降沿，判断高低电平。若判断最后一位存储完毕，该帧结束，回到状态1
-				IR_Time=IRTimer_GetCounter();
-				TIM_SetCounter(TIM2,0);
-				if(IR_Time>1120-500 && IR_Time<1120+500){
-					IR_Data[IR_pData/8]&=~(0x01<<(IR_pData%8));//左移补零故只能用0x01，清零需要0xFE故取反
-					IR_pData++;
-				}else if(IR_Time>2250-500 && IR_Time<2250+500){
-					IR_Data[IR_pData/8]|=(0x01<<(IR_pData%8));//左移补零故只能用0x01，置1不需要取反
-					IR_pData++;
-				}else{
-					IR_pData=0;
-					IR_State=1;
-				}
-				if(IR_pData>=32){
-					IR_pData=0;
-					if((IR_Data[0]==(uint8_t)(~IR_Data[1]))&&(IR_Data[2]==(uint8_t)(~IR_Data[3]))){//强制转换为(uint8_t)，详见C中的按位取反的逻辑
-						IR_Address=IR_Data[0];
-						IR_Command=IR_Data[2];
-						IR_DataFlag=1;
-					}
-					TIM_Cmd(TIM2,DISABLE);
-					IR_State=0;
-				}
-				break;
-			}
-		}
-	}
+    if(EXTI_GetITStatus(EXTI_Line2)==SET){
+        EXTI_ClearITPendingBit(EXTI_Line2);
+        switch(IR_State){
+            case 0:{//第1个下降沿，新的一帧，进入状态1
+                TIM_SetCounter(TIM2,0);
+                TIM_Cmd(TIM2,ENABLE);
+                IR_State=1;
+                break;
+            }
+            case 1:{//第2个下降沿，判断开始或重复。若判断为开始，进入状态2；若判断为重复，该帧结束，回到状态1
+                IR_Time=IRTimer_GetCounter();
+                TIM_SetCounter(TIM2,0);//不关闭计时器，且开始新的计时
+                if(IR_Time>13500-500 && IR_Time<13500+500){
+                    IR_State=2;
+                }else if(IR_Time>11250-500 && IR_Time<11250+500){
+                    IR_RepeatFlag=1;
+                    TIM_Cmd(TIM2,DISABLE);//也可以不停计时器，在状态0会清零重启
+                    IR_State=0;
+                }else{
+                    IR_State=1;
+                }
+                break;
+            }
+            case 2:{//第3个至n个下降沿，判断高低电平。若判断最后一位存储完毕，该帧结束，回到状态1
+                IR_Time=IRTimer_GetCounter();
+                TIM_SetCounter(TIM2,0);
+                if(IR_Time>1120-500 && IR_Time<1120+500){
+                    IR_Data[IR_pData/8]&=~(0x01<<(IR_pData%8));//左移补零故只能用0x01，清零需要0xFE故取反
+                    IR_pData++;
+                }else if(IR_Time>2250-500 && IR_Time<2250+500){
+                    IR_Data[IR_pData/8]|=(0x01<<(IR_pData%8));//左移补零故只能用0x01，置1不需要取反
+                    IR_pData++;
+                }else{
+                    IR_pData=0;
+                    IR_State=1;
+                }
+                if(IR_pData>=32){
+                    IR_pData=0;
+                    if((IR_Data[0]==(uint8_t)(~IR_Data[1]))&&(IR_Data[2]==(uint8_t)(~IR_Data[3]))){//强制转换为(uint8_t)，详见C中的按位取反的逻辑
+                        IR_Address=IR_Data[0];
+                        IR_Command=IR_Data[2];
+                        IR_DataFlag=1;
+                    }
+                    TIM_Cmd(TIM2,DISABLE);
+                    IR_State=0;
+                }
+                break;
+            }
+        }
+    }
 }
